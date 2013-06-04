589baa31ed56f6148a3210ec5215e146611bf30f
ehsan <ehsan@ehsan.(none)>
2013-06-04 20:16:06 +0430

    compile.sh added for easy comilation of project.

---
+++ compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+REQUIREMENTS="libexpat libm libpthread libresolv"
+echo "checking for requirements..."
+for rq in $REQUIREMENTS; do
+	if [ "$(locate $rq)" == "" ]; then
+		echo "$rq required but not located."
+		error=1
+	fi
+done
+
+if [ "$error" == "1" ]; then
+	echo please install requirements then try again.
+	exit
+fi
+
+function compile {
+	echo "compiling..."
+	if [ $(gcc mybot-xmpp.c mybot-functions.c datatypes.c ./modules/responser.c mybot-xmpp.h strophe.h datatypes.h ./modules/responser.h  libstrophe.a -lexpat -lssl -lpcre -lpthread -lresolv -o ./build/mybot-xmpp) ]; then
+		echo "done"
+	else
+		echo "please contact the author for any problem"
+	fi
+}
+
+if [ -d build ]; then
+	compile
+else
+	mkdir build
+	compile
+fi
